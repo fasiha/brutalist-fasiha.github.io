c967d6b4dec690251c288d9c9b2ce10548df2187
Ahmed Fasih <wuzzyview@gmail.com>
2016-05-29 02:38:52 -0400

    More code strings. Trying out read-string for lozs

--- build.cljs
+++ build.cljs
@@ -1,4 +1,6 @@
 #!/usr/bin/env planck
+; fswatch -0 *.html | xargs -0 -n 1 -I {} planck build.cljs
+
 (ns build.core
   (:require [planck.core :refer [slurp spit eval]]
             [clojure.string :as string]
@@ -14,15 +16,24 @@
                    (map (fn [line]
                           (if (re-find #"^ *◊" line)
                             (let [expr (reader/read-string (string/replace line #"◊" ""))]
-                              (eval `(let [~'params ~params] ~expr)))
+                              (planck.repl/eval `(let [~'params ~params] ~expr)
+                                                 (.-name *ns*)))
                             line)))
                    (string/join "\n"))]
     (spit (str output-path (:file params))
           baked)))
 
-(proc {:title "top *** aldebrn.me" :file "index.html"} template-file "baked/")
-(map #(proc % template-file "baked/") [{:title "top *** aldebrn.me" :file "index.html"} {:title "about *** aldebrn.me" :file "about.html"} {:title "code *** aldebrn.me" :file "code.html"}])
+(println "Juicing…")
+(doseq [params [{:title "top *** aldebrn.me" :file "index.html"} 
+                {:title "about *** aldebrn.me" :file "about.html"} 
+                {:title "code *** aldebrn.me" :file "code.html"}]]
+  (proc params template-file "baked/"))
 
 ; (let [bindings '[x 1]] (eval `(let ~bindings ~(reader/read-string "(+ 1 x)"))))
 ; (eval `(let ~'[x 1] ~(reader/read-string "(+ 1 x)")))
 ; (let [params {:x 1}] (eval `(let [~'params ~params] ~(reader/read-string "(+ 1 (:x params))"))))
+
+
+(def s "◊123.213 ... ◊(+ 4 1) ... ◊\"hi\" ...")
+(map (juxt identity (partial read-string {:eof nil})) (string/split s #"◊"))
+
