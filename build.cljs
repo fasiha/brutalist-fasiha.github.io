03a481c47f88e2c6cd75e15f538438b5efa00658
Ahmed Fasih <wuzzyview@gmail.com>
2016-05-29 23:57:58 -0400

    Vastly improved lozenge analysis

--- build.cljs
+++ build.cljs
@@ -2,30 +2,36 @@
 ; fswatch -0 *.html | xargs -0 -n 1 -I {} planck build.cljs
 
 (ns build.core
-  (:require [planck.core :refer [slurp spit eval]]
+  (:require [planck.core :refer [slurp spit]]
+            [planck.repl :refer [eval]]
             [clojure.string :as string]
-            [cljs.tools.reader :as reader]))
+            [cljs.tools.reader :as reader]
+            [cljs.tools.reader.reader-types :as rt]))
 
 (def template-file "template.html")
 
+(defn my-eval [s params]
+  (let [pbr (rt/string-push-back-reader s)
+        expr (reader/read pbr)]
+    (into [(eval `(let [~'params ~params] ~expr)
+                 (.-name *ns*))]
+          (take-while #(not (nil? %))
+                      (repeatedly #(rt/read-char pbr))))))
+(defn evaluate-lozenges [s re params]
+  (let [[f & r] (string/split s re)]
+    (apply str (into [f] (mapcat #(my-eval % params)) r))))
+
 (defn proc [params template-file output-path]
-  (let [template (->> template-file
+  (let [baked (-> template-file
                       slurp
-                      string/split-lines)
-        baked (->> template
-                   (map (fn [line]
-                          (if (re-find #"^ *◊" line)
-                            (let [expr (reader/read-string (string/replace line #"◊" ""))]
-                              (planck.repl/eval `(let [~'params ~params] ~expr)
-                                                 (.-name *ns*)))
-                            line)))
-                   (string/join "\n"))]
+                      (evaluate-lozenges #"(?:^|[^\\])◊"
+                                         params))]
     (spit (str output-path (:file params))
           baked)))
 
 (println "Juicing…")
-(doseq [params [{:title "top *** aldebrn.me" :file "index.html"} 
-                {:title "about *** aldebrn.me" :file "about.html"} 
+(doseq [params [{:title "top *** aldebrn.me" :file "index.html"}
+                {:title "about *** aldebrn.me" :file "about.html"}
                 {:title "code *** aldebrn.me" :file "code.html"}]]
   (proc params template-file "baked/"))
 
@@ -34,6 +40,8 @@
 ; (let [params {:x 1}] (eval `(let [~'params ~params] ~(reader/read-string "(+ 1 (:x params))"))))
 
 
-(def s "◊123.213 ... ◊(+ 4 1) ... ◊\"hi\" ...")
-(map (juxt identity (partial read-string {:eof nil})) (string/split s #"◊"))
+;;; Example:
+
+(def s2 "yo ◊[:b \"Ahmed\"] you are #◊(* 10 (:x params))! Magic via \\◊s.")
+(def res2 (evaluate-lozenges s2 #"(?:^|[^\\])◊" {:x 123})) res2
 
